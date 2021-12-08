9d4b1adc047aaeb0b6299e338875328c6f68d890
Oliver131313 <kutisoliver@outlook.com>
2021-12-08 18:35:42 +0100

    Confusion matrix addition and plot finalization

--- Final_Project/Kutis_Skuska.R
+++ Final_Project/Kutis_Skuska.R
@@ -194,7 +194,7 @@ fifa.fa %>%
     arrange(desc(Off)) %>%
     pivot_longer(-Pos) %>%
     ggplot(aes(x = name, y = value, fill = Pos)) +
-        geom_bar(stat = "identity", position = "dodge")
+    geom_bar(stat = "identity", position = "dodge")
 
 
 # LDA - Aplikovane na nove premenne z FA
@@ -218,7 +218,7 @@ fifa.fa %>%
     geom_histogram(position = "identity",
                    fill = "azure2",
                    alpha = 1) +
-    geom_density(alpha = 1,) +
+    geom_density(alpha = 1, ) +
     facet_grid(Variable ~ .) +
     scale_color_brewer(palette = "Spectral")
 
@@ -232,7 +232,7 @@ fifa.fa <-
 ######## Tato cast moze ostat zakomentovana - Prilis dlho trva urobit 1000 LDAs na
 ########  37 stlpcoch a 10,000 riadkoch. Pointa tejto casti kodu je porovnat ci je lepsie
 ########  urobit LDA na FA pretransormovanych alebo povodnych stlpcoch. T-test udava, ze
-########  AUC pre LDA na povodnych stlpcoch je statisticky vyznamne rozne od LDA 
+########  AUC pre LDA na povodnych stlpcoch je statisticky vyznamne rozne od LDA
 ########  vykonanej na FA transformovanych stlpcoch.
 # # seed
 # set.seed(123)
@@ -243,33 +243,33 @@ fifa.fa <-
 #         fifa.fa$Pos %>% createDataPartition(p = 0.75, list = FALSE)
 #     train.data.fa <- fifa.fa[train.index.fa,]
 #     test.data.fa <- fifa.fa[-train.index.fa,]
-#     
+#
 #     # Fitni model
 #     model.fa <- lda(Pos ~ ., data = train.data.fa,)
-#     
+#
 #     # Predikcie
 #     predictions.fa <- model.fa %>% predict(test.data.fa)
-#     
+#
 #     ## Evaluation
 #     # Mozeme vidiet, ze model je pomerne dobry v tom ako predikuje hodnoty!
 #     predictions.posteriors.fa <-
 #         as.data.frame(predictions.fa$posterior[, 2])
-#     
+#
 #     pred.fa <-
 #         prediction(predictions.posteriors.fa, test.data.fa$Pos)
 #     roc.perform.fa <-
 #         performance(pred.fa, measure = "tpr", x.measure = "fpr")
 #     auc.train.fa <- performance(pred.fa, measure = "auc")
 #     auc.train.fa.val <- auc.train.fa@y.values
-#     
+#
 #     AUC_FA[i] <- as.double(auc.train.fa.val)
 # }
-# 
+#
 # plot(roc.perform.fa)
 # abline(a = 0, b = 1)
 # text(x = .25, y = .65 , paste("AUC = ", round(mean(as.double(AUC_FA)), 3), sep = ""))
-# 
-# 
+#
+#
 # # LDA na raw datach
 # fifa.raw <-
 #     cbind(
@@ -279,33 +279,33 @@ fifa.fa <-
 #         %>% na.omit() %>% dplyr::select(BestPos),
 #         fifa
 #     )
-# 
+#
 # head(fifa.raw)
-# 
+#
 # AUC_RAW <- rep(0, 1000)
 # for (i in 1:1000) {
 #     # Preprocessing dat
 #     preproces.param.raw <-
 #         fifa.raw %>% preProcess(method = c("center", "scale"))
 #     fifa.raw.trans <- preproces.param.raw %>% predict(fifa.raw)
-#     
+#
 #     # Train-test split
 #     train.index.raw <-
 #         fifa.raw$BestPos %>% createDataPartition(p = 0.75, list = FALSE)
 #     train.data.raw <- fifa.raw.trans[train.index.raw, ]
 #     test.data.raw <- fifa.raw.trans[-train.index.raw, ]
-#     
+#
 #     # Fit the model
 #     model.raw <- lda(BestPos ~ ., data = fifa.raw.trans)
-#     
+#
 #     # Predikcie
 #     predictions.raw <- model.raw %>% predict(test.data.raw)
-#     
+#
 #     ## Evaluation
 #     # Mozeme vidiet, ze model je pomerne dobry v tom ako predikuje hodnoty!
 #     predictions.posteriors.raw <-
 #         as.data.frame(predictions.raw$posterior[, 1])
-#     
+#
 #     pred.raw <-
 #         prediction(predictions.posteriors.raw, test.data.raw$BestPos)
 #     roc.perform.raw <-
@@ -313,7 +313,7 @@ fifa.fa <-
 #     auc.train.raw <- performance(pred.raw, measure = "auc")
 #     auc.train.raw.val <- auc.train.raw@y.values
 #     AUC_RAW[i] <- as.double(auc.train.raw.val)
-#     
+#
 # }
 
 # # Porovnanie modelov
@@ -324,11 +324,11 @@ fifa.fa <-
 # LDA na raw datach (Zakomentovat ak chcete vykonat t-test)
 fifa.raw <-
     cbind(
-        data %>% 
-            filter(BestPos %in% c("CM/CAM/CDM", "CF/ST")) %>% 
+        data %>%
+            filter(BestPos %in% c("CM/CAM/CDM", "CF/ST")) %>%
             mutate(BestPos = factor(BestPos, levels = c(
-            "CM/CAM/CDM", "CF/ST"
-        )))
+                "CM/CAM/CDM", "CF/ST"
+            )))
         %>% na.omit() %>% dplyr::select(BestPos),
         fifa
     )
@@ -341,8 +341,8 @@ fifa.raw.trans <- preproces.param.raw %>% predict(fifa.raw)
 # Train-test split
 train.index.raw <-
     fifa.raw$BestPos %>% createDataPartition(p = 0.75, list = FALSE)
-train.data.raw <- fifa.raw.trans[train.index.raw, ]
-test.data.raw <- fifa.raw.trans[-train.index.raw, ]
+train.data.raw <- fifa.raw.trans[train.index.raw,]
+test.data.raw <- fifa.raw.trans[-train.index.raw,]
 
 # Fit the model
 model.raw <- lda(BestPos ~ ., data = train.data.raw)
@@ -372,29 +372,69 @@ text(x = .25, y = .65 , paste("AUC = ", round(AUC_RAW, 3), sep = ""))
 
 
 # Vizualizacia modelu na novych LD osiach
-lda.raw.viz <- as.data.frame(cbind(as.character(test.data.raw$BestPos), 
-                                   predictions.raw$x, 
-                                   as.character(predictions.raw$class))) 
+lda.raw.viz <-
+    as.data.frame(cbind(
+        as.character(test.data.raw$BestPos),
+        predictions.raw$x,
+        as.character(predictions.raw$class)
+    ))
 
 colnames(lda.raw.viz) <- c("Act_class", "LD1", "Pred_class")
 
-lda.raw.viz <- lda.raw.viz %>% 
-        as_tibble() %>%
-        mutate(Pred_OK = as.factor(case_when(Act_class == Pred_class ~ TRUE,
-                               Act_class != Pred_class ~ FALSE)))
+lda.raw.viz <- lda.raw.viz %>%
+    as_tibble() %>%
+    mutate(Pred_OK = as.factor(
+        case_when(Act_class == Pred_class ~ TRUE,
+                  Act_class != Pred_class ~ FALSE)
+    ))
 lda.raw.viz
 
-lda.raw.viz %>%
-ggplot(aes(x=LD1, y=rep(0, length(LD1)))) +
-    geom_jitter(aes(color=Act_class)) + 
-    geom_jitter(data=lda.raw.viz %>%
-                    filter(Pred_OK==FALSE),
-                pch=21,
-                size=3,
-                color="red",
-                fill="red",
-                alpha=0.5) +
-    scale_color_manual(values=c("CM/ST"="black", "CM/CAM/CDM"="blue"))
 
+# Vizualizacia klasifikacie
+library(ggthemes)
+lda.raw.viz %>%
+    ggplot(aes(x = LD1, y = rep(0, length(LD1)))) +
+    geom_jitter(aes(color = Act_class)) +
+    geom_jitter(
+        data = lda.raw.viz %>%
+            filter(Pred_OK == FALSE),
+        pch = 21,
+        size = 3,
+        color = "red",
+        fill = "red",
+        alpha = 0.5
+    ) +
+    scale_color_manual(name= "Skutočné hodnoty",
+                       values = c("CM/ST" = "darkgrey", "CM/CAM/CDM" = "blue"),
+                       ) +
+    theme_economist() + 
+    labs(y = "Jittered 1D",
+         title = "LDA - Predikovanie hráčskej pozície na základe atribútov\n
+         vo futbalovom simulátore FIFA 22"
+    ) + 
+    theme(
+        # Parametre osi
+        axis.ticks.y = element_blank(),
+        axis.line = element_line(color="black"),
+        axis.text = element_blank(),
+        # Grid prec
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.title = element_text(size=15)
+    ) +
+    # Zvacsi velkosti v legende
+    guides(
+        colour = guide_legend(override.aes = list(size=7))
+        )
     
 
+
+# Confusion Matrix
+confusion.mx <- confusionMatrix(
+    data = as.factor(lda.raw.viz$Pred_class),
+    reference = as.factor(lda.raw.viz$Act_class),
+    dnn = c("Prediction", "Reference"),
+)
+
+
+lda.raw.viz
